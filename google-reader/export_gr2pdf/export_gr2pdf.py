9a82db4686a7848157656f62e14d91f3ab08e7df
greatghoul <greatghoul@gmail.com>
2013-03-26 23:03:36 +0800

    Fix chinese font

--- google-reader/export_gr2pdf/export_gr2pdf.py
+++ google-reader/export_gr2pdf/export_gr2pdf.py
@@ -10,28 +10,41 @@ logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 def export_pdf(source_file, target_file):
     logging.info('* Loading source: %s', source_file)
     src_file = codecs.open(source_file, 'r', encoding='utf-8')
-    tmp_file = cStringIO.StringIO()
+    tmp_file = codecs.open(TMP_FILE, 'w', encoding='utf-8')
     data = json.load(src_file, encoding='utf-8')
     count = len(data['items'])
       
     logging.info('  Title: %s', data['title'])
     logging.info('  Total %d entries', count)
     logging.info('  Converting entries to html...')
+    tmp_file.write('''<!DOCTYPE html>
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <link type="text/css" rel="stylesheet" href="style.css" />
+        </head>
+        <body>''')
     tmp_file.write('<h1 style="text-align:center;">%s</h1>\n' % data['title'])
     tmp_file.write('<div style="margin-bottom: 50px;">\n')
+
     for i, item in enumerate(data['items']):
+        if i == 1: break
         logging.info(u'  [%%%dd/%%d] %%s' % len(str(count)) % (i + 1, count, item['title']))
         tmp_file.write('<h2 style="background: #EEE;">%s</h2>\n' % item['title'])
         content = item.get('content', item.get('summary', { 'content': '' }))
         tmp_file.write('<div>%s</div>\n' % content['content'])
         tmp_file.write('</div>\n')
+    tmp_file.write('</body></html>\n')
     tmp_file.close()
     src_file.close()
 
     logging.info('Converting to pdf: %s', target_file)
-    out_file = codecs.open(target_file, 'wb', encoding='utf-8')
+    out_file = codecs.open(target_file, 'wb')
+    tmp_file = codecs.open(TMP_FILE, 'r')
 
-    pdf = xhtml2pdf.pisa.CreatePDF(tmp_file, out_file, encoding='utf-8')
+    pdf = xhtml2pdf.pisa.CreatePDF(tmp_file, out_file, encoding='UTF-8', 
+                                                       link_callback=True,
+                                                       default_css=codecs.open('style.css', 'r', encoding='UTF-8').read().encode('UTF-8'))
     if not pdf.err:
         logging.info('PDF successfull created.')
     else:
@@ -43,7 +56,7 @@ def export_pdf(source_file, target_file):
 if __name__ == '__main__':
     TMP_FILE = 'temp.html'
     SYS_ENCODING = sys.getdefaultencoding()
-    
+
     import argparse
     parser = argparse.ArgumentParser(description='Export google reader to pdf.')
     parser.add_argument('-f', dest='source', required=True, help='Source file to export. starred.json')
