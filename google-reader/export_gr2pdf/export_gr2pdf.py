67f11abb193e7bbb3e443ee6e71516b6a74dd79c
greatghoul <greatghoul@gmail.com>
2013-04-17 13:26:51 +0800

    Update style

--- google-reader/export_gr2pdf/export_gr2pdf.py
+++ google-reader/export_gr2pdf/export_gr2pdf.py
@@ -21,30 +21,53 @@ def export_pdf(source_file, target_file):
     <html>
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <!-- <link rel="stylesheet" type="text/css" href="style.css" />  -->
+            <style type="text/css">
+            @font-face {
+                font-family: simsun;
+                src: url(/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc);
+            }
+            body{
+                font-family: simsun;
+            }
+
+            .article { margin-bottom: 30px; width: 210mm; }
+
+            @page {
+             margin: 1cm;
+             margin-bottom: 2.5cm;
+             font-family: simhei;
+             @frame footer {
+               -pdf-frame-content: footerContent;
+               bottom: 2cm;
+               margin-left: 1cm;
+               margin-right: 1cm;
+               height: 1cm;
+               font-family: simhei;
+             }
+            }
+            </style>
         </head>
         <body>''')
     tmp_file.write('<h1 style="text-align:center;">%s</h1>\n' % data['title'])
-    tmp_file.write('<div style="margin-bottom: 50px;">\n')
 
     for i, item in enumerate(data['items']):
-        if i == 1: break
         logging.info(u'  [%%%dd/%%d] %%s' % len(str(count)) % (i + 1, count, item['title']))
+        tmp_file.write('<div style="article">\n')
         tmp_file.write('<h2 style="background: #EEE;">%s</h2>\n' % item['title'])
         content = item.get('content', item.get('summary', { 'content': '' }))
-        tmp_file.write('<div>%s</div>\n' % content['content'])
+        tmp_file.write('<p>%s</p>\n' % content['content'])
         tmp_file.write('</div>\n')
     tmp_file.write('</body></html>\n')
     tmp_file.close()
     src_file.close()
 
     logging.info('Converting to pdf: %s', target_file)
-    out_file = codecs.open(target_file, 'wb')
-    tmp_file = codecs.open(TMP_FILE, 'rb')
-    css_text = codecs.open(CSS_FILE, 'r').read()
+    out_file = open(target_file, 'wb')
+    tmp_file = open(TMP_FILE, 'rb')
+    # css_text = codecs.open(CSS_FILE, 'r').read()
 
-    pdf = xhtml2pdf.pisa.CreatePDF(tmp_file, out_file, encoding='UTF-8', 
-                                                       link_callback=False,
-                                                       default_css=css_text)
+    pdf = xhtml2pdf.pisa.CreatePDF(tmp_file, out_file, link_callback=False)
     if not pdf.err:
         logging.info('PDF successfull created.')
     else:
